8e0775c73300c4a33c9f3684b89780e0488c54c4
GonzaMaldonado <nahuel.maldonado.gonzalo@gmail.com>
2023-11-24 23:52:42 -0300

    feat: agregando header lateral

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      rotate: {
+        '_45': '-45deg',
+      },
+      colors: {
+        'salmon': '#E7C192',
+        'transparent': 'rgba(0,0,0,0)',
+        'half-transparent': 'rgba(0,0,0, 0.8)',
+      },
+      transitionProperty: {
+        'width': 'width'
+      }
+    },
   },
   plugins: [],
 }
