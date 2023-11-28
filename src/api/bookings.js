dfdacec5dd59b7b738a3b7edf74f221a80565a1e
GonzaMaldonado <nahuel.maldonado.gonzalo@gmail.com>
2023-11-27 21:45:38 -0300

    feat: añadiendo banner

---
+++ src/api/bookings.js
@@ -0,0 +1,39 @@
+import { axi, authAxios } from "./authAxios";
+
+export const bookingsReq = async () => {
+  return await axi.get("/booking/");
+}
+
+export const loginReq = async (username, password) => {
+  const res = await axi.post('/users/login/', {username, password})
+  return res
+}
+
+export const getMeReq = async (id) => {
+  const res = await authAxios.get(`/users/${id}/`)
+  return res?.data
+}
+
+export const updateUserReq = async (id, data) => {
+  const formData = new FormData();
+  formData.append("first_name", data.first_name)
+  formData.append("last_name", data.last_name)
+  formData.append("username", data.username)
+  formData.append("email", data.email)
+  formData.append("birthday", data.birthday)
+  if (data?.image) {
+    formData.append("photo", data.image)
+  }
+  const res = await authAxios.patch(`/users/${id}/`, formData)
+  return res
+}
+
+export const changePasswordReq = async (id, password) => {
+  const res = await authAxios.post(`/change_password/${id}/`, {password})
+  return res
+}
+
+export const deleteUserReq = async (id) => {
+  const res = await authAxios.delete(`/users/${id}/`)
+  return res
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,56 @@
+import { useQuery } from '@tanstack/react-query'
+import { bookingsReq } from '../api/bookings'
+import Loader from '../components/Loader'
+import Banner from '../assets/banner.jpeg'
+
+const baseURL = import.meta.env.VITE_BACKEND_URL
 
 const Home = () => {
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['booking'],
+    queryFn: bookingsReq,
+  });
+
+  let listBookings
+  if (data) {
+    listBookings = data.map(booking => 
+      <article key={booking.id} className="mx-auto justify-center items-center">
+        <div className="h-16 lg:h-20 w-16 lg:w-20 rounded-2xl mx-auto">
+          <img className="h-full w-full object-cover rounded-3xl bg-slate-700" src={`${baseURL}${booking.image}`} alt={booking.name} />
+        </div>
+        <p className='text-center text-white font-bold'>{booking.name}</p>
+      </article>
+    )
+  }
+
+
   return (
-    <div>home</div>
+    <main>
+      <section>
+        <h1 className="text-4xl font-serif font-bold text-center my-2">Haga sus reservas aquí ctm ;)</h1>
+        <div className="relative w-full h-52">
+          <img src={Banner} alt="imagen banner" className="opacity-40 absolute w-full h-full object-cover" />
+          <div className="absolute p-5">
+            <p>Holaaa</p>
+          </div>
+        </div>
+      </section>
+      {isLoading && (<Loader />)}
+      {error ? (
+        <h2 className='text-2xl text-center'>Ocurrio un error al cargar bookings</h2>
+      ) : (
+        <section className="grid grid-cols-2 gap-3 mt-10">
+          {listBookings}
+          <article className="bg-black opacity-50 h-56">A1</article>
+          <article className="bg-black opacity-50 h-56">A2</article>
+          <article className="bg-black opacity-50 h-56">A2</article>
+          <article className="bg-black opacity-50 h-56">A2</article>
+          <article className="bg-black opacity-50 h-56">A2</article>
+          <article className="bg-black opacity-50 h-56">A2</article>
+        </section>
+      )}
+    </main>
   )
 }
 
